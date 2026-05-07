db042a502cde8e204ac8a2ad387aee334c6e61b9
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-05-07 23:24:06 +0800

    feat: please welcome amyc-parser to the workspace

---
+++ amyc-parser/src/lib.rs
@@ -0,0 +1,7 @@
+//! # Amyc Parser
+//!
+//! This library holds the handwritten recursive-descent parser for Amy with error recovery.
+//! Lexical analysis is first performed to produce tokens from a source code string, before that
+//! stream of tokens is parsed into an abstract syntax tree for further processing.
+
+mod lexer;
--- amyc/src/main.rs
+++ amyc/src/main.rs
@@ -1,3 +1,9 @@
+//! # Amyc
+//!
+//! This is the main driver program of the rewritten Amyc. It connects the moving pieces in a
+//! pipeline to drive compilation of Amy into WebAssembly. The concept of a session is used to
+//! store certain global state (per compilation) when necessary.
+
 use tikv_jemallocator::Jemalloc;
 
 #[global_allocator]
