dad57e5be9fc4cd020929ed052263e3dfc749f74
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-05-13 19:34:57 +0800

    implement parts of tokeniter

--- compiler/rac-parser/src/tokeniter.rs
+++ compiler/rac-parser/src/tokeniter.rs
@@ -1,7 +1,6 @@
-
 use crate::{
     // chars::{is_id_continue, is_id_start},
-    token::{Token},
+    token::{Token, TokenKind},
 };
 
 #[derive(Clone, Copy, Debug)]
@@ -10,14 +9,27 @@ pub struct TokenIter<'a> {
     start: usize
 }
 
+impl<'a> TokenIter<'a> {
+    pub fn new(src: &'a str) -> Self {
+        Self { src, start: 0 }
+    }
+}
+
 impl<'a> Iterator for TokenIter<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        panic!()
+        let tok = lex_token(self.src, self.start);
+        self.start += tok.range.len();
+
+        if tok.kind == TokenKind::Eof {
+            None
+        } else {
+            Some(tok)
+        }
     }
 }
 
-fn lex_token(src: &str, start: usize) -> Option<Token> {
+fn lex_token(src: &str, start: usize) -> Token {
     panic!()
 }
