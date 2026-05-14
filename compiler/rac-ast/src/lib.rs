2d4d2fa8a91c2a4eef81f67107a85895377069d5
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-05-14 19:36:17 +0800

    ast: provisionally mapped out the AST structs and enums

--- compiler/rac-ast/src/lib.rs
+++ compiler/rac-ast/src/lib.rs
@@ -1,6 +1,18 @@
 type Name = String;
 
-pub enum AST<N> {
+pub struct Module<N> {
+    name: N,
+    defs: Vec<Definition<N>>,
+    expr: Option<Expr<N>>
+}
+
+pub enum Definition<N> {
+    AbstractDef(N),
+    CaseClassDef(N, Vec<Box<Type<N>>>),
+    FunDef(N, Vec<(N, Type<N>)>, Type<N>, Expr<N>),
+}
+
+pub enum Expr<N> {
     // Variables
     Variable(N),
 
@@ -10,6 +22,53 @@ pub enum AST<N> {
     StringLiteral(String),
     UnitLiteral,
     
-    // Operators
-    Plus(Box<AST<N>>, Box<AST<N>>)
+    // Binary operators
+    Plus(Box<Expr<N>>, Box<Expr<N>>),
+    Minus(Box<Expr<N>>, Box<Expr<N>>),
+    Times(Box<Expr<N>>, Box<Expr<N>>),
+    Div(Box<Expr<N>>, Box<Expr<N>>),
+    Mod(Box<Expr<N>>, Box<Expr<N>>),
+    LessThan(Box<Expr<N>>, Box<Expr<N>>),
+    LessEquals(Box<Expr<N>>, Box<Expr<N>>),
+    And(Box<Expr<N>>, Box<Expr<N>>),
+    Or(Box<Expr<N>>, Box<Expr<N>>),
+    Equals(Box<Expr<N>>, Box<Expr<N>>),
+    Concat(Box<Expr<N>>, Box<Expr<N>>),
+
+    // Unary operators
+    Not(Box<Expr<N>>),
+    Neg(Box<Expr<N>>),
+
+    // Function/constructor call
+    Call(N, Vec<Box<Expr<N>>>),
+
+    // Control flow
+    Sequence(Box<Expr<N>>, Box<Expr<N>>),
+    Let(N, Type<N>, Box<Expr<N>>, Box<Expr<N>>),
+    Ite(Box<Expr<N>>, Box<Expr<N>>, Box<Expr<N>>),
+
+    // Pattern matching
+    Match(Box<Expr<N>>, Vec<(Box<Expr<N>>, Box<Expr<N>>)>),
+
+    // Errors
+    Error(Box<Expr<N>>),
+}
+
+// Do we even need a special pattern enum? we can just say `_` is an
+// expression, and treat patterns as expressions...
+
+// pub enum Pattern<N> {
+//     Wildcard,
+//     IdPattern(N),
+//     
+// }
+
+pub enum Type<N> {
+    // Primitive types
+    IntType,
+    BoolType,
+    StringType,
+    UnitType,
+    // User-defined types
+    ClassType(N),
 }
